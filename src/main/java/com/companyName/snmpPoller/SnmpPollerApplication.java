39697bf7d91e811765b9e689c1f2b0c1a5c7ef28
Shah Alam <shah.alam.mca@gmail.com>
2018-09-13 21:10:00 -0600

    Init commit - Project setup

---
+++ src/main/java/com/companyName/snmpPoller/SnmpPollerApplication.java
@@ -0,0 +1,12 @@
+package com.companyName.snmpPoller.snmppoller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnmpPollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnmpPollerApplication.class, args);
+	}
+}
